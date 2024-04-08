3a50065be1970bf7c8c34c1d16edba7799af1b03
mahdichangizi <mchangizi10@gmail.com>
2024-04-08 12:33:10 +0330

    create job project

--- app/Jobs/SendingSMS.php
+++ app/Jobs/SendingSMS.php
@@ -2,22 +2,34 @@
 
 namespace App\Jobs;
 
+use App\Models\NotifyCode;
+use App\Models\User;
+use App\Notifications\SendVerificationCode;
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeEncrypted;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
-class SendingSMS implements ShouldQueue
+class SendingSMS implements ShouldQueue, ShouldBeEncrypted  # To encrypt errors
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
     /**
      * Create a new job instance.
      */
-    public function __construct()
+
+    public $tries = 5; # Number of attempts to execute
+    public $retryAfter = 10; # Delay time between attempts
+
+
+    public int $code;
+    public function __construct(public User $user)
     {
-        //
+        $this->onQueue("sendingSMS");
     }
 
     /**
@@ -25,6 +37,14 @@ class SendingSMS implements ShouldQueue
      */
     public function handle(): void
     {
-        //
+        DB::beginTransaction();
+        try {
+            $this->code = NotifyCode::generateCode($this->user); # create code
+            $this->user->notify(new SendVerificationCode(code: $this->code, user: $this->user)); #send sms
+            DB::commit();
+        } catch (\Exception $e) {
+            DB::rollBack();
+            Log::error('Error occurred during code generation and SMS sending: ' . $e->getMessage());
+        }
     }
 }
